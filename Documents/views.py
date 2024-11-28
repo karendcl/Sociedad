e068da9619d0c4e2446d8b0fa20111ca64ff8a6a
karendcl <karen.canterolopez@gmail.com>
2024-11-28 16:40:17 -0500

    added view to search form

--- Documents/views.py
+++ Documents/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ApprovedDocuments, PendingDocuments
 
 # Create your views here.
 
 def index(request):
     return render(request, 'base/main.html')
 
+def search(request):
+    documents = ApprovedDocuments.objects.all()
+    if request.method == 'POST':
+        title = request.POST['title']
+        kw = request.POST['keyword']
+
+        all_documents = documents
+        d = []
+
+        for document in all_documents:
+            if title != '' and title not in document.name:
+                continue
+            if kw != '' and kw not in document.text:
+                continue
+            d.append(document)
+
+        documents = d
+    return render(request, 'docs/search.html', {'docs': documents})
